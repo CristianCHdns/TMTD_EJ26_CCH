87511004da5e2d6b39046870172bb103df0ce3f6
CristianCHdns <22030456@itesa.edu.mx>
2026-02-10 13:02:35 -0600

    Seguimiento del Programa 10 / 02 / 2026

---
+++ CapaEntidad/PersonaCLS.cs
@@ -0,0 +1,24 @@
+﻿namespace CapaEntidad
+{
+    public class PersonaCLS
+    {
+        public int iidpersona { get; set; }
+
+        public string nombrecompleto { get; set; }
+
+        public string correo { get; set; }
+
+        public string fechanacimientocadena { get; set; }
+
+        //Recuperar datos
+
+        public string nombre { get; set; }
+        public string appaterno { get; set; }
+        public string apmaterno { get; set; }
+        public DateTime? fechanacimiento { get; set; }
+        public int iidsexo { get; set; }
+
+
+
+    }
+}
